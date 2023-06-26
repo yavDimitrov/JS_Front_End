dcd2181e98aa8c04cdab39a06730ab48b9c907e4
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-06-26 22:51:53 +0300

    arr

--- 03_JS_Syntax_Fundamentals/04_monthPrinter.js
+++ 03_JS_Syntax_Fundamentals/04_monthPrinter.js
@@ -15,6 +15,30 @@ function monthPrinter (num) {
             case 4:
                 console.log("April");
                 break;
+            case 5:
+                    console.log("May");
+                break;
+            case 6:
+                console.log("June");
+                break;
+            case 7:
+                console.log("July");
+                break;
+            case 8:
+                console.log("August");
+                break;
+            case 9:
+                console.log("September");
+                break;
+            case 10:
+                console.log("October");
+                break;
+            case 11:
+                console.log("November");
+                break;
+            case 12:
+                    console.log("December");
+                    break;
         }
     }
 }
---
+++ 03_JS_Syntax_Fundamentals/index_02.js
@@ -0,0 +1,3 @@
+const arr = [1, 2, 3, 4, 5];
+const [first, second, third, fourth, fifth] = arr;
+console.log(first, second ,third, fourth, fifth);
