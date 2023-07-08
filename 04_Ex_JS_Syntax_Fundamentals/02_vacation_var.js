b95f0aba318a8acd99add0c4d404a42d64873f31
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-07-08 22:50:20 +0300

    ex_02_vacation

---
+++ 04_Ex_JS_Syntax_Fundamentals/02_vacation_var.js
@@ -0,0 +1,25 @@
+function solve(count, type, day) {
+   const prices = {
+      Students: {
+         Friday: 8.45,
+         Saturday: 9.8,
+         Sunday: 10.46,
+      },
+      //...
+   };
+
+   let sum = 0;
+   const cost = prices[type][day];
+
+   sum = count * cost;
+
+
+   if(type === `Students` && count >= 30) {
+      sum -= sum*0.15;
+   }
+    
+
+    console.log(`Total price: ${sum.toFixed(2)}`);
+   }
+
+solve(30,"Students", "Sunday");
