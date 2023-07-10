2c4fd8aea98de13ce1d66eac1a3fe7c00f166137
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-07-10 23:53:37 +0300

    ex_09

---
+++ 04_Ex_JS_Syntax_Fundamentals/08_Chars of string_reverse.js
@@ -0,0 +1,5 @@
+function stringByChar_reverse(a, b ,c) {
+    console.log(`${c} ${b} ${a}`);
+}
+
+stringByChar_reverse(`b`,`c`,`a`);
---
+++ 04_Ex_JS_Syntax_Fundamentals/09_friuteCalc.js
@@ -0,0 +1,10 @@
+function calculateFruitPrice (fruit, weightInGrams, pricePerKilogram) {
+    const weightInKilograms =weightInGrams / 1000;
+    const totalPrice = weightInKilograms * pricePerKilogram;
+
+    console.log (
+        `I need ${totalPrice.toFixed(2)} to buy ${weightInKilograms.toFixed(2)} kilograms ${fruit}.`
+    )
+}
+
+calculateFruitPrice(`orabge`, 2500, 1.8)
