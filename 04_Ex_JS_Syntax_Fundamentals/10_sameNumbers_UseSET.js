180dc1348661bee323ddd30dc4d7e0e73259e860
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-07-11 23:47:20 +0300

    ex_10

--- 04_Ex_JS_Syntax_Fundamentals/10_sameNumbers_UseSET.js
+++ 04_Ex_JS_Syntax_Fundamentals/10_sameNumbers_UseSET.js
@@ -4,9 +4,17 @@ function sameNumbersCheck(number) {
     const digits = Array.from(String(number), Number);
     const isConsistent = new Set(digits).size === 1;
 
-    console.log(digits);
+
     console.log(isConsistent);
+
+    if(isConsistent) {
+        const sum = digits.reduce(function (accumulator, current) {
+            return accumulator + current;
+        }, 0);
+
+        console.log(sum);
+    }
 
 }
 
-sameNumbersCheck(2222222);
+sameNumbersCheck(22212222);
