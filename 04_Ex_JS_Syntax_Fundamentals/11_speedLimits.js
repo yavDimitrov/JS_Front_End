faa4224ff514f9331bf721f800bcd53c4743cbe2
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-07-12 23:21:58 +0300

    ex_11

--- 04_Ex_JS_Syntax_Fundamentals/11_speedLimits.js
+++ 04_Ex_JS_Syntax_Fundamentals/11_speedLimits.js
@@ -11,10 +11,22 @@ function checkSpeed (speed, area) {
 
     if(speedOverLimit <= 0) {
         console.log(`Driving ${speed} km/h in a ${currentSpeedLimit} zone`);
+        return;
     }
+
+    const speedingMessage = 
+    speedOverLimit <= 20 
+    ? `speeding` 
+    : speedOverLimit <= 40 
+    ? `excessive speeding` 
+    : `reckless driving`;
+
+    console.log(
+        `The speed is ${speedOverLimit} km/h faster than the allowed speed of ${currentSpeedLimit} - ${speedingMessage}`
+    );
 }
 
-checkSpeed(40, 'city');
-checkSpeed(21, 'residential');
-checkSpeed(120, 'interstate');
-checkSpeed(200, 'motorway');
+checkSpeed (40, 'city');
+checkSpeed (21, 'residential');
+checkSpeed (120, 'interstate');
+checkSpeed (200, 'motorway');
