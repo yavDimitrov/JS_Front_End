a10574cd7949527a7789a5ed1c2bb65974f5cd72
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-07-13 07:48:05 +0300

    ex_13

--- 04_Ex_JS_Syntax_Fundamentals/13_rotateArray.js
+++ 04_Ex_JS_Syntax_Fundamentals/13_rotateArray.js
@@ -1,13 +1,15 @@
 function rotateArray (arr, rotationCount) {
+
+    // kopie na masiva 
+    const myArr=[...arr];
     
     for (let index = 0; index < rotationCount; index++) {
         const firstElement = arr.shift();
         arr.push(firstElement);        
     }
 
-    const newArrString = arr.toString
-
     console.log(arr.join(" "));
+    console.log(myArr.join(" "));
 }
 
-rotateArray([32, 21, 61, 1], 4);
+rotateArray([32, 21, 61, 1], 3);
