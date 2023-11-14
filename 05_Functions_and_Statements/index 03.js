b96418f42447431d9ae7ad61db9aa45fe55ca063
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-11-14 22:48:40 +0200

    continue lesson 02

---
+++ 05_Functions_and_Statements/index 03.js
@@ -0,0 +1,9 @@
+function sortNumbers(...numbers) {
+    console.log("sorting...");
+    console.log(Math.max(1, 2, 3, 4, 5, 10, 5, 60));
+    console.log(Math.min(1, 2, 3, 4, 5, 10, 5, 60));
+
+}
+
+
+sortNumbers(1,2,3,4);
---
+++ 05_Functions_and_Statements/index 04.js
@@ -0,0 +1,7 @@
+function getUpperCaseName(name) {
+    return name.toUpperCase();
+}
+
+
+const result = getUpperCaseName("Alex");
+console.log(result);
