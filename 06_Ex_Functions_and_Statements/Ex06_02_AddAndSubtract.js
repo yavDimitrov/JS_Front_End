3eb6d7365a71646325d5b1a8f18d5e8a6a1c824f
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-11-22 00:27:45 +0200

    Ex 06 Functions and statement

--- 06_Ex_Functions_and_Statements/Ex06_02_AddAndSubtract.js
+++ 06_Ex_Functions_and_Statements/Ex06_02_AddAndSubtract.js
@@ -1,15 +1,12 @@
 function solve (x, y, z) {
     function sum(){
-        return x+y;
+        return x + y;
     }
-    function substract(x ,y) {
-        return x-y;
+    function substract(x, y) {
+        return x - y;
     }
 
-    const sumOfFirstTwo = sum(x,y);
-    const result = substract(sumOfFirstTwo,z);
-
-    return result;
+    return substract(sum(x, y), z);
 }
 
 console.log(solve(23, 6, 10));
