6b6f618f5b44b4baf20859732b8339db7a511ecd
Yavor Dimitrov <yav.dimitrov@gmail.com>
2023-12-02 23:56:45 +0200

    Ex 06 Functions and statement_02

--- 06_Ex_Functions_and_Statements/Ex06-more_02_NumberModification.js
+++ 06_Ex_Functions_and_Statements/Ex06-more_02_NumberModification.js
@@ -1,10 +1,20 @@
 function solve (num) {
     num = num.toString();
 
-    const sum = num.split('')
+    let average =
+    num
+    .split('')
+    .map(Number)
+    .reduce((total, curr) => total +curr, 0) / num.length;
 
-    console.log(sum);
+    while (average < 5) {
+        num += "9";
+        average = num.split("").map(Number).reduce((total, curr) => total + curr, 0) / num.length;
+    }
+
+    return num;
 
 }
 
-solve(101);
+console.log(solve(101));
+console.log(solve(5835));
